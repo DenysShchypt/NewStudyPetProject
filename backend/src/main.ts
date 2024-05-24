002557049655875e33c9f643af659456ccbe3a3c
DenysShchypt <denysshchyptsovit@gmail.com>
2024-05-24 18:10:26 +0300

    remove password frome answear from registration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: ['http://localhost:3000'],
+    },
+  });
   const configService = app.get(ConfigService);
   const port = configService.get('port');
   app.useGlobalPipes(new ValidationPipe());
--- backend/src/modules/auth/responses/index.ts
+++ backend/src/modules/auth/responses/index.ts
@@ -1,13 +1,18 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Exclude, Expose } from 'class-transformer';
 import { IsString } from 'class-validator';
+@Exclude()
 export class UserResponse {
   @ApiProperty()
+  @Expose()
   @IsString()
   firstName: string;
   @ApiProperty()
+  @Expose()
   @IsString()
   lastName: string;
   @ApiProperty()
+  @Expose()
   @IsString()
   email: string;
 }
@@ -15,6 +20,7 @@ export class AuthUserResponse {
   @ApiProperty()
   user: UserResponse;
   @ApiProperty()
+  @Expose()
   @IsString()
   token: string;
 }
