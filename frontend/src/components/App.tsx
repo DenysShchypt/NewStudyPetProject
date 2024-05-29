e0e78b5dc693ac73cbfbd5705602929f0ddaf7d6
DenysShchypt <denysshchyptsovit@gmail.com>
2024-05-29 19:11:10 +0300

    refactoring topBar

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -16,9 +16,10 @@ function App() {
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <LayoutComponent>
-          <div className="app">
-            <Routes>
+
+        <div className="app">
+          <Routes>
+            <Route element={<LayoutComponent />}>
               <Route element={<PrivateRoute />}>
                 <Route path="/" element={<Home />} />
                 <Route path="/watchList" element={<WatchListComponent />} />
@@ -27,9 +28,9 @@ function App() {
               </Route>
               <Route path="login" element={<AuthRootComponent />} />
               <Route path="register" element={<AuthRootComponent />} />
-            </Routes>
-          </div>
-        </LayoutComponent>
+            </Route>
+          </Routes>
+        </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
   );
--- frontend/src/components/TopBar/index.tsx
+++ frontend/src/components/TopBar/index.tsx
@@ -1,44 +1,86 @@
 import { useContext } from 'react';
-import { Box, Grid, IconButton, InputBase, useTheme } from '@mui/material';
-import LightModeSharpIcon from '@mui/icons-material/LightModeSharp';
-import DarkModeSharpIcon from '@mui/icons-material/DarkModeSharp';
-import NotificationsNoneSharpIcon from '@mui/icons-material/NotificationsNoneSharp';
-import SearchSharpIcon from '@mui/icons-material/SearchSharp';
+import {
+  AppBar,
+  Box,
+  Grid,
+  IconButton,
+  InputBase,
+  Toolbar,
+  Typography,
+  useTheme,
+} from '@mui/material';
+import {
+  LightMode,
+  DarkMode,
+  NotificationsNone,
+  Search,
+  MenuOutlined,
+} from '@mui/icons-material';
 import { useAppSelector } from '../../utils/hook';
 import { ColorModeContext } from '../../theme';
 import { useStyles } from './styles';
+import FlexBetween from '../FlexBetween';
 
-const TopBarComponent: React.FC = (): JSX.Element => {
-  const { user } = useAppSelector(state => state.auth);
-  console.log(user);
-
+const TopBarComponent = (props: any) => {
+  const { user } = useAppSelector(state => state.auth.user);
   const theme = useTheme();
   const colorMode: any = useContext(ColorModeContext);
   const classes = useStyles();
+  const { isOpen, setIsOpen } = props;
   return (
-    <Box className={classes.root} position="static">
-      {user && <Grid>Welcome {user.firstName}</Grid>}
-      <Box display="flex">
-        <Grid onClick={colorMode.toggleColorMode} className={classes.iconBlock}>
-          <IconButton className={classes.themeIcon}>
-            {theme.palette.mode === 'dark' ? (
-              <DarkModeSharpIcon />
-            ) : (
-              <LightModeSharpIcon />
-            )}
-          </IconButton>
-          <IconButton>
-            <NotificationsNoneSharpIcon />
-          </IconButton>
-        </Grid>
+    <AppBar className={classes.root} position="static">
+      <Toolbar className={classes.toolbar}>
+        <FlexBetween>
+          <MenuOutlined
+            className={classes.menuIcon}
+            onClick={() => setIsOpen(!isOpen)}
+          />
+          <Typography variant="h3">Welcome {user.firstName}</Typography>
+        </FlexBetween>
+        <Box display="flex">
+          <Grid
+            onClick={colorMode.toggleColorMode}
+            className={classes.iconBlock}
+          >
+            <IconButton className={classes.themeIcon}>
+              {theme.palette.mode === 'dark' ? <DarkMode /> : <LightMode />}
+            </IconButton>
+            <IconButton>
+              <NotificationsNone />
+            </IconButton>
+          </Grid>
+        </Box>
         <Grid className={classes.searchBlock}>
           <IconButton className={classes.searchIcon}>
-            <SearchSharpIcon />
+            <Search />
           </IconButton>
           <InputBase className={classes.searchInput} placeholder="Search" />
         </Grid>
-      </Box>
-    </Box>
+      </Toolbar>
+    </AppBar>
+    // <Box className={classes.root} position="static">
+    //   {user && <Grid>Welcome {user.firstName}</Grid>}
+    //   <Box display="flex">
+    //     <Grid onClick={colorMode.toggleColorMode} className={classes.iconBlock}>
+    //       <IconButton className={classes.themeIcon}>
+    //         {theme.palette.mode === 'dark' ? (
+    //           <DarkModeSharpIcon />
+    //         ) : (
+    //           <LightModeSharpIcon />
+    //         )}
+    //       </IconButton>
+    //       <IconButton>
+    //         <NotificationsNoneSharpIcon />
+    //       </IconButton>
+    //     </Grid>
+    //     <Grid className={classes.searchBlock}>
+    //       <IconButton className={classes.searchIcon}>
+    //         <SearchSharpIcon />
+    //       </IconButton>
+    //       <InputBase className={classes.searchInput} placeholder="Search" />
+    //     </Grid>
+    //   </Box>
+    // </Box>
   );
 };
 
