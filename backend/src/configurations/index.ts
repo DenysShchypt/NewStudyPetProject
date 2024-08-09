2544cccc971c39723b1e61880b5d2d6e63274892
DenysShchypt <denysshchyptsovit@gmail.com>
2024-08-09 23:11:19 +0300

    change dockerLife

--- backend/src/configurations/index.ts
+++ backend/src/configurations/index.ts
@@ -7,14 +7,12 @@ export default (): {
   db_password: string | undefined;
   secret_jwt: string | undefined;
   expire_jwt: string | undefined;
-  refresh_token: string | undefined;
   google_client_id: string | undefined;
   google_client_secret: string | undefined;
   base_url: string | undefined;
   mail_from: string | undefined;
   mail_api_key: string | undefined;
   base_url_client: string | undefined;
-  base_url_client_google: string | undefined;
 } => ({
   port: process.env.PORT,
   db_port: parseInt(process.env.POSTGRES_PORT, 10),
@@ -24,12 +22,10 @@ export default (): {
   db_password: process.env.POSTGRES_PASSWORD,
   secret_jwt: process.env.SECRET_KEY,
   expire_jwt: process.env.EXPIRE_JWT,
-  refresh_token: process.env.REFRESH_TOKEN,
   google_client_id: process.env.GOOGLE_CLIENT_ID,
   google_client_secret: process.env.GOOGLE_CLIENT_SECRET,
   base_url: process.env.BASE_URL,
   mail_from: process.env.MAIL_UKRNET_FROM,
   mail_api_key: process.env.UKRNET_API_KEY,
   base_url_client: process.env.BASE_URL_CLIENT,
-  base_url_client_google: process.env.BASE_URL_CLIENT_GOOGLE,
 });
