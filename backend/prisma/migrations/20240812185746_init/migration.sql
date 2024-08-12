f283596995168401193c8fd3361a143770185991
DenysShchypt <denysshchyptsovit@gmail.com>
2024-08-12 21:59:02 +0300

    prisma change

---
+++ backend/prisma/migrations/20240812185746_init/migration.sql
@@ -0,0 +1,8 @@
+-- DropForeignKey
+ALTER TABLE "RefreshTokin" DROP CONSTRAINT "RefreshTokin_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "WatchList" DROP CONSTRAINT "WatchList_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "tokens" DROP CONSTRAINT "tokens_userId_fkey";
