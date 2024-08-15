b0bb484f60e0c2dfa704224b2e37911b6a6bca60
DenysShchypt <denysshchyptsovit@gmail.com>
2024-08-15 19:11:56 +0300

    fix bug

--- backend/libs/common/middleware/cors.middleware.ts
+++ backend/libs/common/middleware/cors.middleware.ts
@@ -4,7 +4,10 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class CorsMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // Ваш фронтенд URL
+    res.header(
+      'Access-Control-Allow-Origin',
+      'https://new-study-pet-project.vercel.app',
+    ); // Ваш фронтенд URL
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
     res.header(
